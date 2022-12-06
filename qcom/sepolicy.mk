2539002c26e9f9b78af71473fddb116bd41bfd43
Sonal Singh <sonal.singh.19993@gmail.com>
2022-12-06 14:13:46 +0530

    sepolicy: Drop Lineage LiveDisplay policy
    Change-Id: Idcff758444a5c89ba20f2ae804a407ee78c445cb
    Signed-off-by: rajkale99 <kaleraj.rk@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -14,17 +14,11 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/legion/sepolicy/qcom/dynamic \
-    device/legion/sepolicy/qcom/system
+    device/legion/sepolicy/qcom/dynamic
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/legion/sepolicy/qcom/dynamic \
     device/legion/sepolicy/qcom/vendor
-endif
-
-ifneq ($(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/legion/sepolicy/qcom/legacy-vendor
 endif
 
 ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
