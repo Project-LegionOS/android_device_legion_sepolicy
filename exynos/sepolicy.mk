c9899ee236d0e5f1318a75d483c14f05d61148eb
rajkale99 <kaleraj.rk@gmail.com>
2023-04-18 17:36:48 +0530

    sepolicy:LegionOS bringup

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/private
+    device/legion/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/system
+    device/legion/sepolicy/exynos/dynamic \
+    device/legion/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/vendor
+    device/legion/sepolicy/exynos/dynamic \
+    device/legion/sepolicy/exynos/vendor
 endif
